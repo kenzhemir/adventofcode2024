72c1857d364f83d83d9a80e14c5fd86c6d6aa303
Miras <miras.kenzhegaliyev@pipedrive.com>
2024-12-19 15:22:59 +0200

    Day 18

---
+++ day18/part2.js
@@ -0,0 +1,96 @@
+const fs = require("fs");
+function parseInput() {
+  return fs
+    .readFileSync(require("path").join(__dirname, "input.txt"), "utf-8")
+    .split("\n")
+    .map((line) => line.split(",").map(Number));
+}
+
+const size = 71;
+const stepLimit = 1024;
+
+const directions = [
+  [0, 1],
+  [1, 0],
+  [0, -1],
+  [-1, 0],
+];
+
+Array.prototype.tap = function () {
+  console.log(this);
+  return this;
+};
+//-----------
+
+console.log(findNoPath());
+
+//-----------
+
+function findNoPath(input = parseInput()) {
+  return input[
+    binarySearch(
+      stepLimit,
+      input.length,
+      (steps) => !(fallingSim(input, steps) < Infinity)
+    ) - 1
+  ];
+}
+
+function fallingSim(input = parseInput(), stepLimit = stepLimit) {
+  console.log({ stepLimit });
+  const map = new Array(size).fill(0).map(() => new Array(size).fill("."));
+  const dist = new Array(size)
+    .fill(0)
+    .map(() => new Array(size).fill(Infinity));
+
+  for (let i = 0; i < input.length && i < stepLimit; i++) {
+    let [x, y] = input[i];
+    map[y][x] = "#";
+  }
+
+  dist[0][0] = 0;
+  let stack = [{ loc: [0, 0], step: 0 }];
+
+  while (stack.length) {
+    let {
+      loc: [x, y],
+      step,
+    } = stack.pop();
+    if (dist[x][y] < step) continue;
+    if (x === size - 1 && y === size - 1) continue;
+
+    directions
+      .map(([dx, dy]) => [x + dx, y + dy])
+      .filter(([nx, ny]) => withinBoundaries(nx, ny))
+      .filter(([nx, ny]) => map[nx][ny] === ".")
+      //   .tap()
+      .filter(([nx, ny]) => dist[nx][ny] > step + 1)
+      //   .tap()
+      .forEach(([nx, ny]) => {
+        dist[nx][ny] = step + 1;
+        stack.push({ loc: [nx, ny], step: step + 1 });
+      });
+  }
+
+  return dist[size - 1][size - 1];
+}
+
+function withinBoundaries(x, y) {
+  return x >= 0 && x < size && y >= 0 && y < size;
+}
+
+function printMap(map) {
+  console.log(map.map((row) => row.join("")).join("\n"));
+}
+
+function binarySearch(lo, hi, predicate) {
+  while (lo < hi) {
+    let mid = lo + Math.floor((hi - lo) / 2);
+    if (predicate(mid)) {
+      hi = mid;
+    } else {
+      lo = mid + 1;
+    }
+  }
+  return lo;
+}

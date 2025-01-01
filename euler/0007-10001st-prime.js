361cd3c1a0d640885b16467f21e35bcf7e174200
Miras <miras.kenzhegaliyev@pipedrive.com>
2025-01-01 18:48:17 +0200

    --wip-- [skip ci]

--- euler/0007-10001st-prime.js
+++ euler/0007-10001st-prime.js
@@ -3,17 +3,17 @@
 let input = 10001;
 let test_input = 6;
 let test_expected = 13;
-let test_output = nth_prime_number(test_input);
+let test_output = largest_product(test_input);
 if (test_output == test_expected) {
   console.log("✅ Test passed");
-  console.log("10001st prime number: ", nth_prime_number(input));
+  console.log("10001st prime number: ", largest_product(input));
 } else {
   console.log(
     `❌ Test failed. Expected ${test_expected}, but got ${test_output}`
   );
 }
 
-function nth_prime_number(n) {
+function largest_product(n) {
   let generator = prime_number_generator();
   let result = 2;
   for (let i = 0; i < n; i++) {

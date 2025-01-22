2e8f69cf11445d1a82c33e0e06e69bed1d68e063
Miras <miras.kenzhegaliyev@pipedrive.com>
2025-01-22 09:39:57 +0200

    Solve problems 6 and 8

--- euler/0006-sum-square-difference.js
+++ euler/0006-sum-square-difference.js
@@ -1,42 +1,22 @@
-// https://projecteuler.net/problem=5
+// https://projecteuler.net/problem=6
 
-const expectedFor10 = 2520;
-if (smallest_multiple(10) == expectedFor10) {
+const expectedFor10 = 2640;
+if (sum_square_diff(10) == expectedFor10) {
   console.log("✅ Test passed");
-  console.log("Smallest Multiple from 1 to 20: ", smallest_multiple(20));
+  console.log(
+    "difference between the sum of the squares of the first one hundred natural numbers and the square of the sum: ",
+    sum_square_diff(100)
+  );
 } else {
   console.log("❌ Test failed");
 }
 
-function* prime_factors(n) {
-  let d = 2;
-  while (n > 1) {
-    while (n % d === 0) {
-      yield d;
-      n /= d;
-    }
-    d++;
-  }
-}
+function sum_square_diff(n) {
+  let sum = (n * (n + 1)) / 2;
+  let square_of_sum = sum ** 2;
+  let sum_of_squares = new Array(n + 1)
+    .fill(0)
+    .reduce((acc, _, i) => acc + i ** 2, 0);
 
-function smallest_multiple(n) {
-  const factors = new Map();
-  for (let i = 2; i <= n; i++) {
-    const prime_factor_counts = {};
-    for (const f of prime_factors(i)) {
-      prime_factor_counts[f] = (prime_factor_counts[f] ?? 0) + 1;
-    }
-    for (const factor in prime_factor_counts) {
-      factors.set(
-        factor,
-        Math.max(factors.get(factor) ?? 0, prime_factor_counts[factor])
-      );
-    }
-  }
-
-  let result = 1;
-  for (const [factor, count] of factors) {
-    result *= factor ** count;
-  }
-  return result;
+  return square_of_sum - sum_of_squares;
 }
